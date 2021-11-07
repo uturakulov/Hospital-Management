3a831690cb7e1457970eb581c5c960ae8507b419
Ulugbek Turakulov <ulugbek.turaqulov311@gmail.com>
2021-11-07 14:12:17 +0500

    doctor rest api

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Doctor;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Hash;
 
 class DoctorController extends Controller
 {
@@ -13,9 +15,35 @@ class DoctorController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Doctor::all();
+        $doctor = Doctor::latest()->with('category')->with('polyclinic');
+
+        if ($request->first_name) {
+            $doctor->where('first_name', $request->first_name);
+        }
+
+        if ($request->last_name) {
+            $doctor->where('last_name', $request->last_name);
+        }
+
+        if ($request->email) {
+            $doctor->where('email', $request->email);
+        }
+
+        if ($request->created) {
+            $doctor->where('created_at', 'like', '%' . $request->created . '%');
+        }
+
+        if ($request->polyclinic) {
+            $doctor->whereRelation('polyclinic', 'title', 'like', '%' . $request->polyclinic . '%');
+        }
+
+        if ($request->category) {
+            $doctor->whereRelation('category', 'title', 'like', '%' . $request->category . '%');
+        }
+
+        return $doctor->get();
     }
 
     /**
@@ -26,7 +54,28 @@ class DoctorController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        validator($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone_number' => 'required',
+            'category_id' => 'required',
+            'polyclinic_id' => 'required',
+            'email' => ['required', 'email'],
+            'password' => 'required',
+        ])->validate();
+
+        $doctor = new Doctor();
+        $doctor->first_name = $request->first_name;
+        $doctor->last_name = $request->last_name;
+        $doctor->phone_number = $request->phone_number;
+        $doctor->category_id = $request->category_id;
+        $doctor->name = $request->first_name . $request->last_name;
+        $doctor->email = $request->email;
+        $doctor->password = Hash::make($request->password);
+        $doctor->polyclinic_id = $request->polyclinic_id;
+        $doctor->save();
+
+        return $doctor;
     }
 
     /**
@@ -37,7 +86,7 @@ class DoctorController extends Controller
      */
     public function show($id)
     {
-        //
+        return Doctor::with('category')->with('polyclinic')->whereId($id)->get();
     }
 
     /**
@@ -49,7 +98,20 @@ class DoctorController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $doctor = Doctor::findOrFail($id);
+        $doctor->first_name = $request->first_name;
+        $doctor->last_name = $request->last_name;
+        $doctor->phone_number = $request->phone_number;
+        $doctor->category_id = $request->category_id;
+        $doctor->name = $request->first_name . $request->last_name;
+        $doctor->email = $request->email;
+        if ($request->password != null) {
+            $doctor->password = Hash::make($request->password);
+        }
+        $doctor->polyclinic_id = $request->polyclinic_id;
+        $doctor->save();
+
+        return $doctor;
     }
 
     /**
@@ -60,6 +122,10 @@ class DoctorController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $doctor = Doctor::findOrFail($id);
+
+        $doctor->delete();
+
+        return response('Successfully deleted!', 204);
     }
 }
